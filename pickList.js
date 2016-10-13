33e0ebcef25d97c6e49af084cfc9843cb4b62010
Gabriel Rodrigues <gabrielr47@gmail.com>
2016-10-13 09:02:59 -0300

    Multiple Picklists
    now you can instanciate multiple picklists

--- pickList.js
+++ pickList.js
@@ -8,58 +8,63 @@
          var option = '';
 
          $.each(opts.data, function (key, val) {
-            option += '<option id=' + val.id + '>' + val.text + '</option>';
+            option += '<option data-id=' + val.id + '>' + val.text + '</option>';
          });
-         this.find('#pickData').append(option);
+         this.find('.pickData').append(option);
       };
       this.controll = function () {
          var pickThis = this;
 
-         $("#pAdd").on('click', function () {
-            var p = pickThis.find("#pickData option:selected");
-            p.clone().appendTo("#pickListResult");
+         pickThis.find(".pAdd").on('click', function () {
+            var p = pickThis.find(".pickData option:selected");
+            p.clone().appendTo(pickThis.find(".pickListResult"));
             p.remove();
          });
 
-         $("#pAddAll").on('click', function () {
-            var p = pickThis.find("#pickData option");
-            p.clone().appendTo("#pickListResult");
+         pickThis.find(".pAddAll").on('click', function () {
+            var p = pickThis.find(".pickData option");
+            p.clone().appendTo(pickThis.find(".pickListResult"));
             p.remove();
          });
 
-         $("#pRemove").on('click', function () {
-            var p = pickThis.find("#pickListResult option:selected");
-            p.clone().appendTo("#pickData");
+         pickThis.find(".pRemove").on('click', function () {
+            var p = pickThis.find(".pickListResult option:selected");
+            p.clone().appendTo(pickThis.find(".pickData"));
             p.remove();
          });
 
-         $("#pRemoveAll").on('click', function () {
-            var p = pickThis.find("#pickListResult option");
-            p.clone().appendTo("#pickData");
+         pickThis.find(".pRemoveAll").on('click', function () {
+            var p = pickThis.find(".pickListResult option");
+            p.clone().appendTo(pickThis.find(".pickData"));
             p.remove();
          });
       };
+
       this.getValues = function () {
          var objResult = [];
-         this.find("#pickListResult option").each(function () {
-            objResult.push({id: this.id, text: this.text});
+         this.find(".pickListResult option").each(function () {
+            objResult.push({
+               id: $(this).data('id'),
+               text: this.text
+            });
          });
          return objResult;
       };
+
       this.init = function () {
          var pickListHtml =
                  "<div class='row'>" +
                  "  <div class='col-sm-5'>" +
-                 "	 <select class='form-control pickListSelect' id='pickData' multiple></select>" +
+                 "	 <select class='form-control pickListSelect pickData' multiple></select>" +
                  " </div>" +
                  " <div class='col-sm-2 pickListButtons'>" +
-                 "	<button id='pAdd' class='btn btn-primary btn-sm'>" + opts.add + "</button>" +
-                 "      <button id='pAddAll' class='btn btn-primary btn-sm'>" + opts.addAll + "</button>" +
-                 "	<button id='pRemove' class='btn btn-primary btn-sm'>" + opts.remove + "</button>" +
-                 "	<button id='pRemoveAll' class='btn btn-primary btn-sm'>" + opts.removeAll + "</button>" +
+                 "	<button  class='pAdd btn btn-primary btn-sm'>" + opts.add + "</button>" +
+                 "      <button  class='pAddAll btn btn-primary btn-sm'>" + opts.addAll + "</button>" +
+                 "	<button  class='pRemove btn btn-primary btn-sm'>" + opts.remove + "</button>" +
+                 "	<button  class='pRemoveAll btn btn-primary btn-sm'>" + opts.removeAll + "</button>" +
                  " </div>" +
                  " <div class='col-sm-5'>" +
-                 "    <select class='form-control pickListSelect' id='pickListResult' multiple></select>" +
+                 "    <select class='form-control pickListSelect pickListResult' multiple></select>" +
                  " </div>" +
                  "</div>";
 
@@ -82,5 +87,3 @@
 
 
 }(jQuery));
-
-
