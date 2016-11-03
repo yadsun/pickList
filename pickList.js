59dbfdfa663de91d098edb5927e308adf8d5b8b2
yadsun <yadongsun1984@gmail.com>
2016-11-03 09:25:06 +0800

    Update pickList.js
    1.fixed a bug :when pickList is contained in a form block,form submit will be triggered when any button(add,remove,addAll or removeAll) is clicked,change label ''<button' to "<input type='button' can fix this problem
    2.bind double click event to each options in source select box and result select box

--- pickList.js
+++ pickList.js
@@ -3,40 +3,70 @@
    $.fn.pickList = function (options) {
 
       var opts = $.extend({}, $.fn.pickList.defaults, options);
-
+      
+      this.bindDblClickAdd = function () {
+         var pickThis = this;
+         this.find('.pickData option').each(function () {
+            var _option = this;
+            $(_option).off('dblclick');
+            $(_option).on('dblclick', function(){pickThis.fnAdd();});
+         });
+      };
+      this.bindDblClickRemove = function () {
+         var pickThis = this;
+         this.find('.pickListResult option').each(function () {
+            var _option = this;
+            $(_option).off('dblclick');
+            $(_option).on('dblclick', function(){pickThis.fnRemove();});
+         });
+      };
+      this.fnAdd = function () {
+         var pickThis = this;
+         var p = pickThis.find(".pickData option:selected");
+         p.clone().appendTo(pickThis.find(".pickListResult"));
+         p.remove();
+         pickThis.bindDblClickRemove();
+      };
+      this.fnRemove = function () {
+         var pickThis = this;
+         var p = pickThis.find(".pickListResult option:selected");
+         p.clone().appendTo(pickThis.find(".pickData"));
+         p.remove();
+         pickThis.bindDblClickAdd();
+      };
       this.fill = function () {
+         var pickThis = this;
          var option = '';
 
          $.each(opts.data, function (key, val) {
             option += '<option data-id=' + val.id + '>' + val.text + '</option>';
          });
          this.find('.pickData').append(option);
+         this.bindDblClickAdd();
       };
+      
+      
       this.controll = function () {
          var pickThis = this;
 
-         pickThis.find(".pAdd").on('click', function () {
-            var p = pickThis.find(".pickData option:selected");
-            p.clone().appendTo(pickThis.find(".pickListResult"));
-            p.remove();
-         });
+         pickThis.find(".pAdd").on('click', function(){pickThis.fnAdd();});
 
          pickThis.find(".pAddAll").on('click', function () {
             var p = pickThis.find(".pickData option");
             p.clone().appendTo(pickThis.find(".pickListResult"));
             p.remove();
+            pickThis.bindDblClickRemove();
          });
 
          pickThis.find(".pRemove").on('click', function () {
-            var p = pickThis.find(".pickListResult option:selected");
-            p.clone().appendTo(pickThis.find(".pickData"));
-            p.remove();
+            pickThis.fnRemove();
          });
 
          pickThis.find(".pRemoveAll").on('click', function () {
             var p = pickThis.find(".pickListResult option");
             p.clone().appendTo(pickThis.find(".pickData"));
             p.remove();
+            pickThis.bindDblClickAdd();
          });
       };
 
@@ -58,10 +88,10 @@
                  "	 <select class='form-control pickListSelect pickData' multiple></select>" +
                  " </div>" +
                  " <div class='col-sm-2 pickListButtons'>" +
-                 "	<button  class='pAdd btn btn-primary btn-sm'>" + opts.add + "</button>" +
-                 "      <button  class='pAddAll btn btn-primary btn-sm'>" + opts.addAll + "</button>" +
-                 "	<button  class='pRemove btn btn-primary btn-sm'>" + opts.remove + "</button>" +
-                 "	<button  class='pRemoveAll btn btn-primary btn-sm'>" + opts.removeAll + "</button>" +
+                 "	<input type='button' class='pAdd btn btn-primary btn-sm' value='"+opts.add+"' />" +
+                 "      <input type='button'  class='pAddAll btn btn-primary btn-sm' value='"+opts.addAll+"'/>" +
+                 "	<input type='button'  class='pRemove btn btn-primary btn-sm' value='"+opts.remove+"' />" +
+                 "	<input type='button'  class='pRemoveAll btn btn-primary btn-sm' value='"+opts.removeAll+"'/>" +
                  " </div>" +
                  " <div class='col-sm-5'>" +
                  "    <select class='form-control pickListSelect pickListResult' multiple></select>" +
